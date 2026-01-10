de139185ab2cdfa0d4472d63ae43d697d64692bb
Vitaswift <82334055+PitaAuPoulet@users.noreply.github.com>
2026-01-10 08:57:22 -0500

    Update config.lua

--- configs/config.lua
+++ configs/config.lua
@@ -1,7 +1,10 @@
 Config = {}
 
-Config.LimitSpeed = 140 -- (km/)
-Config.StressAdd = 1 --
-Config.MaxStress = 200 -- 
-Config.CheckTime = 1000 -- (1000ms = 1sec)
-Config.Debug = true
+Config.Debug = true            -- Affiche les logs dans la console (F8)
+Config.CheckTime = 1000        -- Vérification chaque seconde (1000ms)
+
+Config.LimitSpeed = 140        -- Vitesse (km/h) à partir de laquelle on stresse
+Config.MaxStress = 100         -- Limite maximale de stress
+Config.StressAdd = 2           -- Points de stress ajoutés par seconde de vitesse
+
+Config.HighStressLevel = 70    -- Seuil pour déclencher l'effet de flou (FocusOut)
